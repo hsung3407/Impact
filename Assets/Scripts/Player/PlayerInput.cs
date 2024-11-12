bd5d0b6c94721b80bbfad4bac27d83ef1e016588
hsung3407 <azxccxzavv3407@gmail.com>
2024-11-12 18:14:53 +0900

    Update PlayerInput.cs

--- Assets/Scripts/Player/PlayerInput.cs
+++ Assets/Scripts/Player/PlayerInput.cs
@@ -4,14 +4,18 @@ namespace Player
 {
     public class PlayerInput
     {
-        public Vector3 GetDirection()
+        public Vector3 Direction { get; private set; } = Vector3.zero;
+
+        public void UpdateInput()
         {
             var dir = Vector3.zero;
             if(Input.GetKey(KeyCode.W)) dir.z += 1;
             if(Input.GetKey(KeyCode.S)) dir.z -= 1;
             if(Input.GetKey(KeyCode.D)) dir.x += 1;
             if(Input.GetKey(KeyCode.A)) dir.x -= 1;
-            return dir.normalized;
+            dir = dir.normalized;
+            var alpha = Mathf.Clamp01(5f * Time.deltaTime / Vector3.Distance(Direction, dir));
+            Direction = Vector3.Lerp(Direction, dir, alpha);
         }
     }
 }
